cbeb354b3ce6cffb3088688ada37b5fff9025136
IKala4I <vpersko@gmail.com>
2023-07-29 16:15:58 +0300

    * Moved updateObjectInArray.js into helpers

--- src/tableHandlers/noteHandlers/unarchiveNote.js
+++ src/tableHandlers/noteHandlers/unarchiveNote.js
@@ -1,6 +1,6 @@
 import {data} from '../../../initialData.js'
 import {rerenderApp} from '../../index.js'
-import {updateObjectInArray} from './updateObjectInArray.js'
+import {updateObjectInArray} from '../../helpers/updateObjectInArray.js'
 
 export const unarchive = (noteId) => {
     data.notes = updateObjectInArray(data.notes, noteId, 'id', {archived: false})
