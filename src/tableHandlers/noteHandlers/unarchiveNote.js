1e0b68d0d06cbb2e3c755f6e9b9feaed0e84adb5
IKala4I <vpersko@gmail.com>
2023-07-29 13:48:07 +0300

    * Fixed bug

--- src/tableHandlers/noteHandlers/unarchiveNote.js
+++ src/tableHandlers/noteHandlers/unarchiveNote.js
@@ -1,6 +1,6 @@
 import {data} from '../../../initialData.js'
 import {rerenderApp} from '../../index.js'
-import {updateObjectInArray} from './updateObjectInArray'
+import {updateObjectInArray} from './updateObjectInArray.js'
 
 export const unarchive = (noteId) => {
     data.notes = updateObjectInArray(data.notes, noteId, 'id', {archived: false})
