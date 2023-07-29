cee54f40c99be148b3828fe44656242d336b49f7
IKala4I <vpersko@gmail.com>
2023-07-29 14:49:37 +0300

    * Removed duplicate styles for action buttons

--- src/tableHandlers/toggleVisibilityArchivedNotes.js
+++ src/tableHandlers/toggleVisibilityArchivedNotes.js
@@ -1,11 +1,9 @@
 export const toggleVisibilityArchivedNotes = button => {
-    debugger
     const notes = document.querySelector('.archived-notes')
     button.onclick = () => toggleArchived(button, notes)
 }
 
 const toggleArchived = (button, notes) => {
-    debugger
     if (notes.style.display === 'block') {
         notes.style.display = 'none'
         button.textContent = 'Show archived notes'
